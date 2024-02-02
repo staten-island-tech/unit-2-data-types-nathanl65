a909da67e7b667ed632d4247ddae9c9f57883378
nathanl65 <158204596+nathanl65@users.noreply.github.com>
2024-02-02 12:28:37 -0500

    AAA

---
+++ data.py
@@ -0,0 +1,43 @@
+#Data Types 
+#Nuymbers 1,2,3,
+""" def add(x,y):
+    print(x+y)
+add(1,2)
+#strings "a,b,c"
+name = "Mark"
+def greeting(person):
+    print(person)
+greeting(name)
+#1 and "1" are not the same
+add("1","2")
+#undefined/nuLL
+
+
+#booleans
+tenure = False
+def is_tenured(status):
+    if(status == True):
+        print("They have tenure")
+    else:
+        print("They are not tenured")
+
+is_tenured(tenure)
+
+x = 3
+y = float(3)
+print(x,y)
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+        print(i)
+
+values = ["w","e","s","t"]
+print(values[0])
+print(values[3])"""
+
+x = "this is a thing"
+y= x.split( )
+z = y[1]
+print(y)
+print(z)
